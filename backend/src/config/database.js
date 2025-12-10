53917a2cf7d5b0c81d9f13a27f6d2af3ce9d216f
wiissal <wissaloa1@gmail.com>
2025-12-10 15:50:15 +0100

    [Backend] Install Sequelize and create database config

---
+++ backend/src/config/database.js
@@ -0,0 +1,13 @@
+const {Sequelize} = require('sequelize');
+require('dotenv').config();
+
+const sequelize = new Sequelize({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  dialect: 'postgres',
+  logging: false,
+});
+module.exports = sequelize;
