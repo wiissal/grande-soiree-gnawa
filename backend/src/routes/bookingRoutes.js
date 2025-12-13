b7a5d4d5be76c8c0c0299554e8bafe29fbb6c413
wiissal <wissaloa1@gmail.com>
2025-12-13 14:07:01 +0100

    create route file for bookings

---
+++ backend/src/routes/bookingRoutes.js
@@ -0,0 +1,12 @@
+const express =require('express');
+const router = express.Router();
+const { createBooking , getBookingByCode , getBookingsByEmail, createBooking} = require('../controllers/bookingController');
+
+//New Booking
+router.post('/',createBooking);
+//get booking by confirmation code 
+router.get('/:code', getBookingByCode);
+// get booking by email
+router.get('/email/:email', getBookingsByEmail);
+
+module.exports = router;
