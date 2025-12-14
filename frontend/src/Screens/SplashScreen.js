463b2c2db8193f72989511a7c8a73235c7ed7bd6
wiissal <wissaloa1@gmail.com>
2025-12-14 15:18:25 +0100

    fix: setup App.js and index.js for Expo

---
+++ frontend/src/Screens/SplashScreen.js
@@ -0,0 +1,29 @@
+import React, { use, useEffect } from "react";
+import { View, Image, StyleSheet } from "react-native";
+import { colors } from "../constants/colors";
+
+export default function SplashScreen({ navigation }) {
+  useEffect(() => {
+    setTimeout(() => {
+      navigation.replace("Home");
+    }, 5000);
+  }, [navigation]);
+  return (
+    <View style={styles.container}>
+      <Image source={require("../../assets/splash.png")} style={styles.image} />
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: colors.deepTeal,
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'cover',
+  },
+});
