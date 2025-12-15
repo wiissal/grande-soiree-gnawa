d36f357f7f8ed34134533267d263598115d18220
wiissal <wissaloa1@gmail.com>
2025-12-15 21:05:48 +0100

    complete MyBookingsScreen with all UI screens done

--- frontend/src/navigation/Navigator.js
+++ frontend/src/navigation/Navigator.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import  { useState, useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
